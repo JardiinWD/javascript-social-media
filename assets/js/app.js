a9d27866ee1fee6ba086c4effb2419162814b51b
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-08-29 17:15:09 +0200

    Creato filtro messaggi nella barra di ricerca

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,49 @@
 //#region Variabili
 export const menuItems = querySelectorAll('.menu-item')
+export const messagesNotification = querySelector('#messages-notifications')
+export const messages = querySelector('.messages')
+export const message = messages.querySelectorAll('.message')
+export const messageSearch = document.querySelector('#message-search')
 //#endregion 
 
 
 //#region Import
 import { querySelectorAll } from './function.js'; // Funzione per il querySelector, valida per tutte
+import { querySelector } from './function.js'; // Rimuove classe Active su Tutte
 import { changeActiveItem } from './function.js'; // Rimuove classe Active su Tutte
+import { searchMessage } from './function.js'; // Rimuove classe Active su Tutte
 
 //#endregion
 
-//#region Cicli
+//#region SideBar laterale
 menuItems.forEach(element => {
     element.addEventListener('click', () => {
         changeActiveItem() // Invoco la mia funzione per rimuovere la classe active su tutti
         element.classList.add('active') // Aggiungo la classe active solo sul richiesto
+        if (element.id != 'notificationsPopup') {
+            querySelector('.notifications-popup').style.display = 'none' // Il mio menu a popup rimane nascosto
+        } else {
+            querySelector('.notifications-popup').style.display = 'block' // Faccio comparire il menu a popup
+            querySelector('#notificationsPopup .notification-count').style.display = 'none' // Faccio scomparire il numero di notifiche
+        }
     })
 })
 
-
-
 //#endregion
 
+//#region Messages
+
+// Highlight presente dietro il box al click
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 0.5rem hsl(252, 75%, 60%)' // Aggiungo il colore a tutto il box dei messaggi
+    messagesNotification.querySelector('.notification-count').style.display = 'none'
+    // Faccio scomparire dopo 2 secondi il mio box shadow
+    setTimeout(() => {
+        messages.style.boxShadow = 'none'
+    }, 2000)
+})
+
+// Ricerca Chat
+messageSearch.addEventListener('keyup', searchMessage)
+
+//#endregion
--- assets/js/function.js
+++ assets/js/function.js
@@ -1,10 +1,13 @@
 //#region Imports
-import { menuItems } from './app.js'; // Variabile in App.js
+import { menuItems } from './app.js';
+import { messageSearch } from './app.js';
+import { message } from './app.js';
+
 
 //#endregion
 
-/**
- * Funzione per rimuovere stato attivo non necessario
+/** Funzione per rimuovere stato attivo non necessario
+ * 
  */
 export function changeActiveItem() {
     menuItems.forEach(element => {
@@ -12,10 +15,36 @@ export function changeActiveItem() {
     })
 }
 
-/**
+/** Funzione per leggere tutti i querySelector
  * 
- * @param {string} element Classe da inserire nel query Selector
+ * @param {string} element Classe/id da inserire nel query Selector
  */
 export function querySelectorAll(element) {
     return document.querySelectorAll(element)
+}
+
+/** Funzione per leggere querySelector specifici
+ * 
+ * @param {string} element Classe/id da inserire nel query Selector
+ * @returns 
+ */
+export function querySelector(element) {
+    return document.querySelector(element)
+}
+
+/** Funzione per filtrare messaggi
+ * 
+ */
+export const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase() // Salvo l'input dell'utente
+    console.log(val); // Verifico in console
+    // Eseguo un loop su tutti i messaggi della chat
+    message.forEach(chat => {
+        let name = chat.querySelector('h5').textContent.toLowerCase() // Selezioni i miei nomi
+        if (name.indexOf(val) != -1) {
+            chat.style.display = 'flex'
+        } else {
+            chat.style.display = 'none'
+        }
+    })
 }
